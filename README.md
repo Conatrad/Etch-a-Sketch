428d8ce962a7e3b9bf959c4d5ab813b8a78ca62c
Sorru <conatrad@gmail.com>
2020-01-04 15:01:11 -0500

    Program is now functional and has updated styles. Clear button has been added and README.md has been updated.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Etch-a-Sketch
+# Pixel-Sketch
+
+This is an Etch-a-Sketch playeed within the browser. 
+Hover your mouse over the pixels you would like to color, and when you're finished press the clear button to reset the grid.
--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 generateGrid = () => {
-    
-const div = document.createElement('div');
-const section = document.querySelector('section');
 
-    for(let i = 0; i < 16; i++)
+const container = document.querySelector('.container');
+
+    for(let h = 0; h < 16; h++)
     {
-        div.classList.add('row');
-        div.style.cssText = "background-color: red";
-        div.textContent = ".";
-        div.appendChild(section);
+        for(let i = 0; i < 16; i++)
+        {  
+            let div = document.createElement('div');
+            div.classList.add('grid', 'red');
+            container.appendChild(div);
+        }
     }
 
 }
+
 generateGrid();
+
+let pixel = document.querySelectorAll('div');
+
+pixel.forEach(function(pixel){
+    pixel.addEventListener('mouseover', () => {
+        pixel.classList.remove('red');
+    })
+})
+
+let clear = document.querySelector('#clear');
+
+clear.addEventListener('click', () => {
+    pixel.forEach(function (pixel){ 
+        pixel.classList.add('red'); 
+    });
+});

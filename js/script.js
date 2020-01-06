ee88923a9395baf2fb6d1f4a98a2fc521553f08f
Sorru <Conatrad@gmail.com>
2020-01-06 15:23:25 -0500

    updated resize button and pixel creation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
+let size  = 16;
+size = size > 50 ? size = 50 : size = size;
+size = size < 1 ? size = 1 : size = size;
+
+
 generateGrid = () => {
 
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
+let main = document.querySelector('.main');
 
-    for(let h = 0; h < 16; h++)
+    for(let h = 0; h < size; h++)
     {
-        for(let i = 0; i < 16; i++)
+        for(let i = 0; i < size; i++)
         {  
             let div = document.createElement('div');
             div.classList.add('grid', 'red');
             container.appendChild(div);
         }
     }
-
+    container.style.cssText = 'grid-template-columns: repeat(' + size + ', 1fr); grid-template-rows: repeat(' + size + ', 1fr);'
+    main.appendChild(container);
 }
 
 generateGrid();
@@ -25,9 +32,15 @@ pixel.forEach(function(pixel){
 })
 
 let clear = document.querySelector('#clear');
+let resize = document.querySelector('#resize');
 
 clear.addEventListener('click', () => {
     pixel.forEach(function (pixel){ 
         pixel.classList.add('red'); 
     });
-});
+})
+
+resize.addEventListener('click', () => {
+    size = prompt('How many pixels would you like the canvas to be? \n Pick a number between 1 and 50');
+    generateGrid();
+})

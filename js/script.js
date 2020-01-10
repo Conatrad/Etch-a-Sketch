d84cf24bb40900a4bab8cfe5dbaddeab55a8890d
Sorru <conatrad@gmail.com>
2020-01-10 13:07:33 -0500

    fixed small resize bug

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-let size  = 16;
-size = size > 50 ? size = 50 : size = size;
-size = size < 1 ? size = 1 : size = size;
-
-
-generateGrid = () => {
-
+let size = 16;
 let container = document.querySelector('.container');
 let main = document.querySelector('.main');
+
+generateGrid = () => {
 
     for(let h = 0; h < size; h++)
     {
@@ -17,30 +13,44 @@ let main = document.querySelector('.main');
             container.appendChild(div);
         }
     }
-    container.style.cssText = 'grid-template-columns: repeat(' + size + ', 1fr); grid-template-rows: repeat(' + size + ', 1fr);'
-    main.appendChild(container);
+    print();
 }
 
 generateGrid();
 
-let pixel = document.querySelectorAll('div');
 
-pixel.forEach(function(pixel){
-    pixel.addEventListener('mouseover', () => {
-        pixel.classList.remove('red');
-    })
+function hover(){
+
+    let pixel = document.querySelectorAll('div');
+    pixel.forEach(function(pixel){
+        pixel.addEventListener('mouseover', () => {
+            pixel.classList.remove('red');
+        })
 })
+return pixel;
+}
 
-let clear = document.querySelector('#clear');
+function print() {
+    container.style.cssText = 'grid-template-columns: repeat(' + size + ', 1fr); grid-template-rows: repeat(' + size + ', 1fr);'
+    main.appendChild(container);
+    hover();
+}
+
+let clearBtn = document.querySelector('#clear');
 let resize = document.querySelector('#resize');
 
-clear.addEventListener('click', () => {
-    pixel.forEach(function (pixel){ 
-        pixel.classList.add('red'); 
-    });
-})
+clearBtn.addEventListener('click', clear);
 
 resize.addEventListener('click', () => {
     size = prompt('How many pixels would you like the canvas to be? \n Pick a number between 1 and 50');
+    size = size > 50 ? size = 50 : size = size;
+    size = size < 1 ? size = 1 : size = size;
     generateGrid();
+    clear();
 })
+
+function clear(){
+    pixel.forEach(function (pixel){ 
+        pixel.classList.add('red'); 
+    })
+}
